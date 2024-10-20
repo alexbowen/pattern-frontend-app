825c241075469761625d1b856c9b576b88acceff
alexbowen <alexbowen123@googlemail.com>
2024-10-20 20:00:00 +0100

    remove unused files

--- app/shows/[id]/_page.tsx
+++
@@ -1,9 +0,0 @@
-import List from '../../components/episodes/dataSet'
-
-export default async function Browse({ params }: { params: { id: string } }) {
-  return (
-    <div className="container-fluid">
-      <List offset={ 0 } limit={ 6 } q={ params.id.trim().toLowerCase().replace(/-/g, '+') } />
-    </div>
-  )
-}
