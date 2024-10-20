7db4ed2b786cd801d49ac1fb0894559c1fdc3028
alexbowen <alexbowen123@googlemail.com>
2024-10-21 00:40:45 +0100

    hardcode remote host to force deploy

--- app/components/tabs.tsx
+++ app/components/tabs.tsx
@@ -1,33 +1,33 @@
 'use client'
 
-import styles from './styles/tabs.module.scss';
+import styles from './styles/tabs.module.scss'
 
-import { useTabs } from './tabsContext';
+import { useTabs } from './tabsContext'
 
 import Tab from './tab'
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 export default function Tabs() {
 
-  const [cart] = useTabs();
+  const [cart] = useTabs()
 
-  const [ active, setActive ] = useState(cart.active);
+  const [ active, setActive ] = useState(cart.active)
 
   useEffect(() => {
     if (cart.active) {
-      // localStorage.setItem('pattern-radio-style', cart.active.toString());
+      window.localStorage.setItem('pattern-radio-style', cart.active.toString())
       setActive(cart.active)
     }
-  }, [cart]);
+  }, [cart])
 
   return (
     <ul className={ styles.tabs } role="tablist" data-tabs-active={ active ? true : false }>
       <li>
-    <Tab title="Electronic sounds" subTitle="House, Techno, Electronica, Dubstep, Broken Beat, Electro, Breaks, Downtempo" categoryId="21" />
+    <Tab title="Electronic sounds" subTitle="House, Techno, Electronica, Dubstep, Broken Beat, Electro, Breaks, Downtempo" categoryId="21" active={ active === '21' } />
     </li>
     <li>
-    <Tab title="Organic sounds" subTitle="Jazz, Funk, Soul, World, Latin, Afro, Disco, Dub, Reggae, Punk" categoryId="25" />
+    <Tab title="Organic sounds" subTitle="Jazz, Funk, Soul, World, Latin, Afro, Disco, Dub, Reggae, Punk" categoryId="25" active={ active === '25' } />
     </li>
   </ul>
   )
