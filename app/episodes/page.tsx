e79b5ccf30997de8315757344e3af56de7dfbd51
alexbowen <alexbowen123@googlemail.com>
2024-10-23 00:05:36 +0100

    further proto tweaks

--- app/episodes/page.tsx
+++ app/episodes/page.tsx
@@ -7,18 +7,30 @@ export default function Browse({ searchParams }) {
 
   return (
     <div>
-    <div className="container-fluid content content__primary content--theme-dark">
-      <div className="content__container">
-        <h1 className="font-dark" data-heading-target="title">{searchParams.q ? `Search Results for '${searchParams.q}'` : 'Browse Shows'}</h1>
-        <List offset={ 0 } limit={6} q={searchParams.q} pagination_control={ true } />
+      <div className="container-fluid content content__primary content--theme-dark">
+        <div className="content__container">
+          <h1 className="font-dark" data-heading-target="title">{searchParams.q ? `Search Results for '${searchParams.q}'` : 'Browse Shows'}</h1>
+          <List offset={0} limit={6} q={searchParams.q} pagination_control={true} />
+        </div>
+        <span className="content__background"></span>
       </div>
-      <span className="content__background"></span>
-    </div>
 
-<div className="container-fluid content content--theme-light">
-<h2 className="font-dark">Related Reading</h2>
-<Posts heading_text={'Related reading'} episode_context={ []} per_page={2} exclude={0} template={'feature'} offset={2} categories={[20]} tax_relation={'OR'} />
-</div>
-</div>
-  )
+      <div className="container-fluid content content--theme-dark">
+
+        <div className="row">
+
+          <div className="col-12 col-md-6">
+            <h2 className="font-dark">Reviews & Releases</h2>
+            <Posts offset={0} per_page={2} exclude={0} categories={[39]} tax_relation={'AND'} tabs_context={true} episode_context={[]} template={'feature'} />
+          </div>
+
+          <div className="col-12 col-md-6">
+            <h2 className="font-dark">Whats On</h2>
+            <Posts offset={0} per_page={2} exclude={0} categories={[33]} tax_relation={'AND'} tabs_context={true} episode_context={[]} template={'feature'} />
+          </div>
+        </div>
+
+      </div>
+      </div>
+      )
 }
